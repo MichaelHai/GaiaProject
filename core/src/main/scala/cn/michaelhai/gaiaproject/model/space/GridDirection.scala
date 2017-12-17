926bf542c6f8add90b5c98f962d82d8cb1b4b337
MichaelHai <michael.wanghai@outlook.com>
2017-12-18 02:18:58 +0800

    Issue #8 Model board elements add neighbour relative direction

--- core/src/main/scala/cn/michaelhai/gaiaproject/model/space/GridDirection.scala
+++ core/src/main/scala/cn/michaelhai/gaiaproject/model/space/GridDirection.scala
@@ -2,28 +2,62 @@ package cn.michaelhai.gaiaproject.model.space
 
 sealed trait GridDirection {
   def oppositeDirection: GridDirection
+
+  def neighbourRelativeDirections(): Seq[RelativeDirection]
 }
+
+case class RelativeDirection(neighbourDirection: GridDirection, relativeDirection: GridDirection)
 
 case class UP() extends GridDirection {
   override def oppositeDirection: GridDirection = DOWN()
+
+  override def neighbourRelativeDirections(): Seq[RelativeDirection] = Seq(
+    RelativeDirection(UP_LEFT(), UP_RIGHT()),
+    RelativeDirection(UP_RIGHT(), UP_LEFT())
+  )
 }
 
 case class UP_RIGHT() extends GridDirection {
   override def oppositeDirection: GridDirection = DOWN_LEFT()
+
+  override def neighbourRelativeDirections(): Seq[RelativeDirection] = Seq(
+    RelativeDirection(UP(), UP_LEFT()),
+    RelativeDirection(DOWN_RIGHT(), DOWN())
+  )
 }
 
 case class DOWN_RIGHT() extends GridDirection {
   override def oppositeDirection: GridDirection = UP_LEFT()
+
+  override def neighbourRelativeDirections(): Seq[RelativeDirection] = Seq(
+    RelativeDirection(UP_RIGHT(), UP()),
+    RelativeDirection(DOWN(), DOWN_LEFT())
+  )
 }
 
 case class DOWN() extends GridDirection {
   override def oppositeDirection: GridDirection = UP()
+
+  override def neighbourRelativeDirections(): Seq[RelativeDirection] = Seq(
+    RelativeDirection(DOWN_RIGHT(), UP_RIGHT()),
+    RelativeDirection(DOWN_LEFT(), UP_LEFT())
+  )
 }
 
 case class DOWN_LEFT() extends GridDirection {
   override def oppositeDirection: GridDirection = UP_RIGHT()
+
+  override def neighbourRelativeDirections(): Seq[RelativeDirection] = Seq(
+    RelativeDirection(DOWN(), DOWN_LEFT()),
+    RelativeDirection(UP_LEFT(), UP())
+  )
 }
 
 case class UP_LEFT() extends GridDirection {
   override def oppositeDirection: GridDirection = DOWN_RIGHT()
+
+  override def neighbourRelativeDirections(): Seq[RelativeDirection] = Seq(
+    RelativeDirection(DOWN_LEFT(), DOWN()),
+    RelativeDirection(UP(), UP_RIGHT())
+  )
 }
